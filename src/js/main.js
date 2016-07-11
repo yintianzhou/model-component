20a91d8fd431ef711ad1ac4e1eda337e39361dd2
yintianzhou <478189271@qq.com>
2016-07-11 14:53:45 +0800

    add function

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,19 @@ require.config({
 
 require(['jquery', 'window'], function($, w) {
     $("#alertBtn").on("click", function () {
-        new w.Window().alert("alert!");
+        new w.Window().alert({
+            width: 300,
+            height: 150,
+            y: 50,
+            title: "提示",
+            content: "welcome",
+            handler4AlertBtn: function() {
+                alert("click");
+            },
+            handler4CloseBtn: function () {
+                alert("close");
+            },
+            hasCloseBtn: true
+        });
     })
 });
--- src/js/window.js
+++ src/js/window.js
@@ -3,14 +3,56 @@
  */
 define(['jquery'], function($) {
     function Window() {
-
+        this.config = {
+            width: 500,
+            height: 300,
+            content: "",
+            title: "系统消息",
+            skinClassName: null,
+            handler4AlertBtn: null,
+            handler4CloseBtn: null,
+            hasCloseBtn: false
+        };
     }
     
     Window.prototype = {
-        alert: function (content) {
-            var alertBox = $('<div class="alert-box"></div>');
+        alert: function (config) {
+            var CFG = $.extend(this.config, config);
+
+            var alertBox = $(
+                '<div class="alert-box">' +
+                    '<div class="alert-header">' + CFG.title + '</div>' +
+                    '<div class="alert-body">' + CFG.content + '</div>' +
+                    '<div class="alert-footer"><input class="alert-btn" type="button" value="确定"/></div>' +
+                '</div>'
+            );
             alertBox.appendTo("body");
-            alertBox.html(content);
+
+            var btn = alertBox.find(".alert-footer input");
+            btn.on("click", function() {
+                CFG.handler4AlertBtn && CFG.handler4AlertBtn();
+                alertBox.remove();
+            });
+
+            alertBox.css({
+                width: this.config.width + "px",
+                height: this.config.height + "px",
+                left: (this.config.x || (window.innerWidth - this.config.width)/2) + "px",
+                top: (this.config.y || (window.innerHeight - this.config.height)/2) + "px"
+            });
+
+            if(CFG.hasCloseBtn) {
+                var closeBtn = $('<span class="alert-closeBtn">X</span>');
+                closeBtn.appendTo(alertBox);
+                closeBtn.on("click", function() {
+                    CFG.handler4CloseBtn && CFG.handler4CloseBtn();
+                    alertBox.remove();
+                });
+            }
+
+            if(CFG.skinClassName) {
+                alertBox.addClass(CFG.skinClassName);
+            }
         },
         confirm: function () {
 
